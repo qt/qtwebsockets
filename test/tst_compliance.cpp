871cdd6858e155b492e0f63cf616f60caa562a06
Kurt Pattyn <kurt.pattyn@barco.com>
2013-08-06 14:57:41 +0200

    Cleaned up code

--- test/tst_compliance.cpp
+++ test/tst_compliance.cpp
@@ -8,49 +8,39 @@
 
 class ComplianceTest : public QObject
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    ComplianceTest();
+	ComplianceTest();
 
 private Q_SLOTS:
-    void initTestCase();
-    void cleanupTestCase();
-    void init();
-    void cleanup();
-    /**
-     * @brief Runs the autobahn tests against our implementation
-     */
-    void autobahnTest();
+	void initTestCase();
+	void cleanupTestCase();
+	void init();
+	void cleanup();
+	/**
+	 * @brief Runs the autobahn tests against our implementation
+	 */
+	void autobahnTest();
 
 private:
-    WebSocket *m_pWebSocket;
-    QUrl m_url;
+	QUrl m_url;
+
+	void runTestCases(int startNbr, int stopNbr = -1);
+	void runTestCase(int nbr, int total);
 };
 
 ComplianceTest::ComplianceTest() :
-    m_pWebSocket(0),
-    m_url("ws://localhost:9001")
+	m_url("ws://localhost:9001")
 {
 }
 
 void ComplianceTest::initTestCase()
 {
-    //m_pWebSocket = new WebSocket(WebSocketProtocol::V_LATEST);
-//    m_pWebSocket->open(m_url, true);
-//    QTRY_VERIFY_WITH_TIMEOUT(m_pWebSocket->state() == QAbstractSocket::ConnectedState, 1000);
-//    QVERIFY(m_pWebSocket->isValid());
 }
 
 void ComplianceTest::cleanupTestCase()
 {
-//    if (m_pWebSocket)
-//    {
-//        m_pWebSocket->close();
-//        //QVERIFY2(m_pWebSocket->waitForDisconnected(1000), "Disconnection failed.");
-//        delete m_pWebSocket;
-//        m_pWebSocket = 0;
-//    }
 }
 
 void ComplianceTest::init()
@@ -61,67 +51,80 @@ void ComplianceTest::cleanup()
 {
 }
 
-void ComplianceTest::autobahnTest()
+void ComplianceTest::runTestCase(int nbr, int total)
 {
-    //connect to autobahn server at url ws://ipaddress:port/getCaseCount
-    WebSocket *pWebSocket = new WebSocket;
-    int numberOfTestCases = 0;
-    QString m;
-    QObject::connect(pWebSocket, &WebSocket::textFrameReceived, [&](QString message, bool) {
-        //qDebug() << "Received text message from autobahn:" << message;
-        numberOfTestCases = message.toInt();
-    });
-    QObject::connect(pWebSocket, &WebSocket::aboutToClose, [=]() {
-        //qDebug() << "About to close";
-        //pWebSocket->deleteLater();
-    });
+	if (nbr == total)
+	{
+		return;
+	}
+	WebSocket *pWebSocket = new WebSocket;
+	QSignalSpy spy(pWebSocket, SIGNAL(disconnected()));
 
-    QUrl url = m_url;
-//    url.setPort(9001);
-    url.setPath("/getCaseCount");
-    pWebSocket->open(url);
-    QTRY_VERIFY_WITH_TIMEOUT(pWebSocket->state() == QAbstractSocket::UnconnectedState, 1000);
-    QVERIFY(numberOfTestCases > 0);
-    QObject::disconnect(pWebSocket, &WebSocket::textFrameReceived, 0, 0);
+	//next for every case, connect to url
+	//ws://ipaddress:port/runCase?case=<number>&agent=<agentname>
+	//where agent name will be QWebSocket
+	QObject::connect(pWebSocket, &WebSocket::textMessageReceived, [=](QString message) {
+		pWebSocket->send(message);
+	});
+	QObject::connect(pWebSocket, &WebSocket::binaryMessageReceived, [=](QByteArray message) {
+		pWebSocket->send(message);
+	});
 
-    //next for every case, connect to url
-    //ws://ipaddress:port/runCase?case=<number>&agent=<agentname>
-    //where agent name will be QtWebSocket
-    QObject::connect(pWebSocket, &WebSocket::textFrameReceived, [=](QString message, bool) {
-        //qDebug() << "Received text message from autobahn:" << message.length();
-        pWebSocket->send(message);
-        //pWebSocket->close();
-    });
-    QObject::connect(pWebSocket, &WebSocket::binaryFrameReceived, [=](QByteArray message, bool) {
-        //qDebug() << "Received binary message from autobahn:" << message;
-        pWebSocket->send(message);
-        //pWebSocket->close();
-    });
-
-    for (int i = 0; i < numberOfTestCases; ++i)
-    //for (int i = 16; i < 28; ++i)
-    {
-        qDebug() << "Executing test" << (i + 1) << "/" << numberOfTestCases;
-        url.setPath("/runCase?");
-        QUrlQuery query;
-        query.addQueryItem("case", QString::number(i + 1));
-        query.addQueryItem("agent", "QtWebSockets");
-        url.setQuery(query);
-
-        pWebSocket->open(url);
-        QTRY_VERIFY_WITH_TIMEOUT(pWebSocket->state() == QAbstractSocket::UnconnectedState, 60000);
-    }
-
-    url.setPath("/updateReports?");
-    QUrlQuery query;
-    query.addQueryItem("agent", "QtWebSockets");
-    url.setQuery(query);
-    pWebSocket->open(url);
-    QTRY_VERIFY_WITH_TIMEOUT(pWebSocket->state() == QAbstractSocket::UnconnectedState, 60000);
-    pWebSocket->close();
+	qDebug() << "Executing test" << (nbr + 1) << "/" << total;
+	QUrl url = m_url;
+	url.setPath("/runCase?");
+	QUrlQuery query;
+	query.addQueryItem("case", QString::number(nbr + 1));
+	query.addQueryItem("agent", "QWebSockets(optimized)");
+	url.setQuery(query);
+	pWebSocket->open(url);
+	spy.wait(60000);
+	//pWebSocket->close();
+	delete pWebSocket;
+	runTestCase(nbr + 1, total);
 }
 
-DECLARE_TEST(ComplianceTest);
+void ComplianceTest::runTestCases(int startNbr, int stopNbr)
+{
+	runTestCase(startNbr, stopNbr);
+}
+
+void ComplianceTest::autobahnTest()
+{
+	//connect to autobahn server at url ws://ipaddress:port/getCaseCount
+	WebSocket *pWebSocket = new WebSocket;
+	QUrl url = m_url;
+	int numberOfTestCases = 0;
+	QSignalSpy spy(pWebSocket, SIGNAL(disconnected()));
+	QObject::connect(pWebSocket, &WebSocket::textMessageReceived, [&](QString message) {
+		numberOfTestCases = message.toInt();
+	});
+//	QObject::connect(pWebSocket, &WebSocket::disconnected, [=]() {
+//	});
+
+	url.setPath("/getCaseCount");
+	pWebSocket->open(url);
+	spy.wait(60000);
+	QVERIFY(numberOfTestCases > 0);
+
+	QObject::disconnect(pWebSocket, &WebSocket::textMessageReceived, 0, 0);
+
+	//runTestCases(64 + 7, 64+141);	//141
+	//runTestCases(64 + 33, 64+33 + 2);	//141
+	//runTestCases(73, 77);	//testcase 6.4.1 - 6.4.4
+	runTestCases(0, numberOfTestCases);
+	//runTestCases(78, 79);	//testcase 6.6.1
+
+	url.setPath("/updateReports?");
+	QUrlQuery query;
+	query.addQueryItem("agent", "QWebSockets");
+	url.setQuery(query);
+	pWebSocket->open(url);
+	spy.wait(60000);
+	delete pWebSocket;
+}
+
+DECLARE_TEST(ComplianceTest)
 
 #include "tst_compliance.moc"
 
