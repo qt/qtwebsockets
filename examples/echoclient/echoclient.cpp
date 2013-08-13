0228a9413439533e3b67ce7a7637df25a66ff8b2
Kurt Pattyn <pattyn.kurt@gmail.com>
2013-08-13 14:49:28 +0200

    Added doxygen snippet tags

--- examples/echoclient/echoclient.cpp
+++ examples/echoclient/echoclient.cpp
@@ -1,6 +1,7 @@
 #include "echoclient.h"
 #include <QDebug>
 
+//! [constructor]
 EchoClient::EchoClient(const QUrl &url, QObject *parent) :
 	QObject(parent),
 	m_webSocket()
@@ -8,15 +9,20 @@ EchoClient::EchoClient(const QUrl &url, QObject *parent) :
 	connect(&m_webSocket, SIGNAL(connected()), this, SLOT(onConnected()));
 	m_webSocket.open(QUrl(url));
 }
+//! [constructor]
 
+//! [onConnected]
 void EchoClient::onConnected()
 {
 	qDebug() << "Websocket connected";
 	connect(&m_webSocket, SIGNAL(textMessageReceived(QString)), this, SLOT(onTextMessageReceived(QString)));
 	m_webSocket.send("Hello, world!");
 }
+//! [onConnected]
 
+//! [onTextMessageReceived]
 void EchoClient::onTextMessageReceived(QString message)
 {
 	qDebug() << "Message received:" << message;
 }
+//! [onTextMessageReceived]
