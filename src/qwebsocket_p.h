4de3b54c254b0e11efae82f3e7fc70da664ffd80
Kurt Pattyn <pattyn.kurt@gmail.com>
2013-08-25 01:04:23 +0200

    Added warning comment to private header files

--- src/qwebsocket_p.h
+++ src/qwebsocket_p.h
@@ -10,6 +10,16 @@
 
 #ifndef QWEBSOCKET_P_H
 #define QWEBSOCKET_P_H
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
 
 #include <QUrl>
 #include <QAbstractSocket>
