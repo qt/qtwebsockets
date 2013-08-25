839c91c0444397a400c4b6f0133f8f81502db184
Kurt Pattyn <pattyn.kurt@gmail.com>
2013-08-25 19:14:09 +0200

    Added copyright disclaimer to all files

--- src/qwebsocket_p.h
+++ src/qwebsocket_p.h
@@ -1,13 +1,21 @@
-/**
- * @file websocket.h
- * @brief Defines the WebSocket class.
- *
- * \note Currently, only V13 (RFC6455) is supported.
- * \note Both text and binary websockets are supported.
- * \note The secure version (wss) is currently not implemented.
- * @author Kurt Pattyn (pattyn.kurt@gmail.com)
- */
+/*!
+QWebSockets implements the WebSocket protocol as defined in RFC 6455.
+Copyright (C) 2013 Kurt Pattyn (pattyn.kurt@gmail.com)
 
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 #ifndef QWEBSOCKET_P_H
 #define QWEBSOCKET_P_H
 //
