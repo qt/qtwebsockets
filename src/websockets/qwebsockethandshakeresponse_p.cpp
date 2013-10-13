fa963706938337cb4e1589c5c6ab2cfabc46b9ea
Kurt Pattyn <pattyn.kurt@gmail.com>
2013-10-13 21:17:53 +0200

    Replaced getter methods of HandshakeResponse to be in line with Qt
    Also added additional check in QWebSocketServer for valid request
    
    Change-Id: I4c524864f71cd35488aabb1f8a919948d11f4d90

--- src/websockets/qwebsockethandshakeresponse_p.cpp
+++ src/websockets/qwebsockethandshakeresponse_p.cpp
@@ -77,7 +77,7 @@ bool QWebSocketHandshakeResponse::canUpgrade() const
 /*!
     \internal
  */
-QString QWebSocketHandshakeResponse::getAcceptedProtocol() const
+QString QWebSocketHandshakeResponse::acceptedProtocol() const
 {
     return m_acceptedProtocol;
 }
@@ -96,11 +96,11 @@ QString QWebSocketHandshakeResponse::calculateAcceptKey(const QString &key) cons
     \internal
  */
 QString QWebSocketHandshakeResponse::getHandshakeResponse(const QWebSocketHandshakeRequest &request,
-                                                const QString &serverName,
-                                                bool isOriginAllowed,
-                                                const QList<QWebSocketProtocol::Version> &supportedVersions,
-                                                const QList<QString> &supportedProtocols,
-                                                const QList<QString> &supportedExtensions)
+                                                          const QString &serverName,
+                                                          bool isOriginAllowed,
+                                                          const QList<QWebSocketProtocol::Version> &supportedVersions,
+                                                          const QList<QString> &supportedProtocols,
+                                                          const QList<QString> &supportedExtensions)
 {
     QStringList response;
     m_canUpgrade = false;
@@ -204,7 +204,7 @@ QTextStream &operator <<(QTextStream &stream, const QWebSocketHandshakeResponse 
 /*!
     \internal
  */
-QWebSocketProtocol::Version QWebSocketHandshakeResponse::getAcceptedVersion() const
+QWebSocketProtocol::Version QWebSocketHandshakeResponse::acceptedVersion() const
 {
     return m_acceptedVersion;
 }
@@ -212,7 +212,7 @@ QWebSocketProtocol::Version QWebSocketHandshakeResponse::getAcceptedVersion() co
 /*!
     \internal
  */
-QString QWebSocketHandshakeResponse::getAcceptedExtension() const
+QString QWebSocketHandshakeResponse::acceptedExtension() const
 {
     return m_acceptedExtension;
 }
