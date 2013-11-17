b402246836d7fd7d102813400081c2c4e830c9c2
Kurt Pattyn <pattyn.kurt@gmail.com>
2013-11-17 15:20:08 +0100

    Add QML websockets plugin
    Change-Id: I9454cf339f8af5515d3a91667d8c8ded3659d18b

--- src/imports/qmlwebsockets/qqmlwebsocket.h
+++ src/imports/qmlwebsockets/qqmlwebsocket.h
@@ -44,6 +44,9 @@
 
 #include <QObject>
 #include <QQmlParserStatus>
+#include <QtQml>
+#include <QScopedPointer>
+#include <QWebSocket>
 
 class QQmlWebSocket : public QObject, public QQmlParserStatus
 {
@@ -51,12 +54,64 @@ class QQmlWebSocket : public QObject, public QQmlParserStatus
     Q_DISABLE_COPY(QQmlWebSocket)
     Q_INTERFACES(QQmlParserStatus)
 
+    Q_ENUMS(Status)
+    Q_PROPERTY(QUrl url READ url WRITE setUrl NOTIFY urlChanged)
+    Q_PROPERTY(Status status READ status NOTIFY statusChanged)
+    Q_PROPERTY(QString errorString READ errorString)
+    Q_PROPERTY(bool active READ isActive WRITE setActive NOTIFY activeChanged)
+
 public:
     explicit QQmlWebSocket(QObject *parent = Q_NULLPTR);
+    virtual ~QQmlWebSocket();
+
+    enum Status
+    {
+        Connecting  = 0,
+        Open        = 1,
+        Closing     = 2,
+        Closed      = 3,
+        Error       = 4
+    };
+
+    QUrl url() const;
+    void setUrl(const QUrl &url);
+    Status status() const;
+    QString errorString() const;
+
+    void setActive(bool active);
+    bool isActive() const;
+
+public Q_SLOTS:
+    void sendTextMessage(const QString &message);
+    //void sendBinaryMessage(const QByteArray &message);
+
+
+Q_SIGNALS:
+    void textMessageReceived(QString message);
+    //void binaryMessageReceived(QByteArray message);
+    void statusChanged(Status status);
+    void activeChanged(bool isActive);
+    void urlChanged();
 
 public:
     void classBegin() Q_DECL_OVERRIDE;
     void componentComplete() Q_DECL_OVERRIDE;
+
+private Q_SLOTS:
+    void onError(QAbstractSocket::SocketError error);
+    void onStateChanged(QAbstractSocket::SocketState state);
+
+private:
+    QScopedPointer<QWebSocket> m_webSocket;
+    Status m_status;
+    QUrl m_url;
+    bool m_isActive;
+    bool m_componentCompleted;
+    QString m_errorString;
+
+    void setStatus(Status status);
+    void open();
+    void close();
 };
 
 #endif // QQMLWEBSOCKET_H
