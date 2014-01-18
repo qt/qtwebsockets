856956c51e31f102d80a91801579461444d8ef68
Kurt Pattyn <pattyn.kurt@gmail.com>
2014-01-18 16:21:39 +0100

    Remove dubious functions
    Change-Id: Ida5c46e3e42685f5e78f2aee657d12af62286810
    Reviewed-by: Kurt Pattyn <pattyn.kurt@gmail.com>
    Reviewed-by: Frederik Gladhorn <frederik.gladhorn@digia.com>

--- src/websockets/qwebsocket_p.h
+++ src/websockets/qwebsocket_p.h
@@ -140,8 +140,6 @@ public:
     QWebSocketProtocol::CloseCode closeCode() const;
     QString closeReason() const;
 
-    qint64 write(const char *message) Q_REQUIRED_RESULT;
-    qint64 write(const char *message, qint64 maxSize) Q_REQUIRED_RESULT;
     qint64 write(const QString &message) Q_REQUIRED_RESULT;
     qint64 write(const QByteArray &data) Q_REQUIRED_RESULT;    //send data as binary
 
