8ebed733e872ded507256addf932e19e1b6ed2b6
Liang Qi <liang.qi@theqtcompany.com>
2015-02-13 11:31:52 +0100

    Examples: add options for port and debug outputs in echoserver
    Change-Id: Iadba1c5cb7cd4454b01a98339c2225483b08e180
    Reviewed-by: Kurt Pattyn <pattyn.kurt@gmail.com>
    Reviewed-by: Venugopal Shivashankar <venugopal.shivashankar@digia.com>
    Reviewed-by: Topi Reiniö <topi.reinio@digia.com>

--- examples/websockets/echoserver/main.cpp
+++ examples/websockets/echoserver/main.cpp
@@ -31,12 +31,30 @@
 **
 ****************************************************************************/
 #include <QtCore/QCoreApplication>
+#include <QtCore/QCommandLineParser>
+#include <QtCore/QCommandLineOption>
 #include "echoserver.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    EchoServer *server = new EchoServer(1234);
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("QtWebSockets example: echoserver");
+    parser.addHelpOption();
+
+    QCommandLineOption dbgOption(QStringList() << "d" << "debug",
+            QCoreApplication::translate("main", "Debug output [default: off]."));
+    parser.addOption(dbgOption);
+    QCommandLineOption portOption(QStringList() << "p" << "port",
+            QCoreApplication::translate("main", "Port for echoserver [default: 1234]."),
+            QCoreApplication::translate("main", "port"), QLatin1Literal("1234"));
+    parser.addOption(portOption);
+    parser.process(a);
+    bool debug = parser.isSet(dbgOption);
+    int port = parser.value(portOption).toInt();
+
+    EchoServer *server = new EchoServer(port, debug);
     QObject::connect(server, &EchoServer::closed, &a, &QCoreApplication::quit);
 
     return a.exec();
