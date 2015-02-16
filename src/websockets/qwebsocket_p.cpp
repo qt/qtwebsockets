fc2b42c874fb41487b86700a04e932e2afaded94
Liang Qi <liang.qi@theqtcompany.com>
2015-02-16 11:01:40 +0100

    client ping should be masked
    Task-number: QTBUG-42298
    Change-Id: I4c01052823ce48b737d30ac2e0ef11836124bc4f
    Reviewed-by: Kurt Pattyn <pattyn.kurt@gmail.com>
    Reviewed-by: Neil Williams
    Reviewed-by: Liang Qi <liang.qi@theqtcompany.com>

--- src/websockets/qwebsocket_p.cpp
+++ src/websockets/qwebsocket_p.cpp
@@ -440,8 +440,13 @@ void QWebSocketPrivate::ping(const QByteArray &payload)
 {
     QByteArray payloadTruncated = payload.left(125);
     m_pingTimer.restart();
+    quint32 maskingKey = 0;
+    if (m_mustMask)
+        maskingKey = generateMaskingKey();
     QByteArray pingFrame = getFrameHeader(QWebSocketProtocol::OpCodePing, payloadTruncated.size(),
-                                          0 /*do not mask*/, true);
+                                          maskingKey, true);
+    if (m_mustMask)
+        QWebSocketProtocol::mask(&payloadTruncated, maskingKey);
     pingFrame.append(payloadTruncated);
     qint64 ret = writeFrame(pingFrame);
     Q_UNUSED(ret);
