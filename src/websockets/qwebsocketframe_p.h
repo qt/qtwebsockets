7784b52c31d50ab81597c38bcf55b63e2f1a4f82
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-10-05 11:12:12 +0200

    Add missing "We mean it" comments to private headers.
    Change-Id: Icc8c6928619b2e16f5f13e852e559438faf9a03c
    Reviewed-by: Liang Qi <liang.qi@theqtcompany.com>

--- src/websockets/qwebsocketframe_p.h
+++ src/websockets/qwebsocketframe_p.h
@@ -34,6 +34,17 @@
 #ifndef QWEBSOCKETFRAME_P_H
 #define QWEBSOCKETFRAME_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtCore/QString>
 #include <QtCore/QByteArray>
 #include <QtCore/QCoreApplication>
