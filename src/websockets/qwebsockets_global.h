09d456e169204e21ee73fec830e6ce49c1a4d2c1
Alexey Edelev <alexey.edelev@qt.io>
2021-06-28 16:33:43 +0200

    Migrate to autogenerated cpp exports
    Replace the hardcoded cpp exports with a generated one where it's
    applicable.
    
    Task-number: QTBUG-90492
    Change-Id: Idc2d2d9f7ba2abbff462a27599b9a7b1aa704a4f
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- src/websockets/qwebsockets_global.h
+++ src/websockets/qwebsockets_global.h
@@ -41,18 +41,6 @@
 #define QWEBSOCKETSGLOBAL_H
 
 #include <QtCore/qglobal.h>
+#include <QtWebSockets/qtwebsocketsexports.h>
 
-QT_BEGIN_NAMESPACE
-
-#ifndef QT_STATIC
-#  if defined(QT_BUILD_WEBSOCKETS_LIB)
-#    define Q_WEBSOCKETS_EXPORT Q_DECL_EXPORT
-#  else
-#    define Q_WEBSOCKETS_EXPORT Q_DECL_IMPORT
-#  endif
-#else
-#  define Q_WEBSOCKETS_EXPORT
-#endif
-
-QT_END_NAMESPACE
 #endif // QWEBSOCKETSGLOBAL_H
