dce25ec66db622f3e4545ef870cec394d8b5c374
Iikka Eklund <iikka.eklund@qt.io>
2021-06-28 12:31:31 +0300

    Support builds with Conan
    Provide conanfile.py recipe to support building this module
    with Conan. The conanfile.py recipe inherits the main
    functionality from base class located in qt-conan-common.git.
    
    'qt-configure-module(.bat)' and cmake is used for the builds.
    
    Dependencies (Conan requirements) are defined by the base
    class implementation which reads those from the
    'dependencies.yaml' which is also used by the Coin/CI.
    
    Pick-to: 6.2
    Task-number: QTBUG-94825
    Change-Id: Ieeaa8d9c0bf44d86318753fc6b4ffe2fa14f7dc9
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>
    Reviewed-by: Toni Saario <toni.saario@qt.io>

---
+++ conanfile.py
@@ -0,0 +1,62 @@
+#############################################################################
+##
+## Copyright (C) 2021 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the release tools of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:GPL-EXCEPT$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 3 as published by the Free Software
+## Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+from conans import ConanFile
+import re
+from pathlib import Path
+
+
+def _parse_qt_version_by_key(key: str) -> str:
+    with open(Path(__file__).parent.resolve() / ".cmake.conf") as f:
+        m = re.search(fr'{key} .*"(.*)"', f.read())
+    return m.group(1) if m else ""
+
+
+def _get_qt_minor_version() -> str:
+    return ".".join(_parse_qt_version_by_key("QT_REPO_MODULE_VERSION").split(".")[:2])
+
+
+class QtWebSockets(ConanFile):
+    name = "qtwebsockets"
+    license = "LGPL-3.0, GPL-2.0+, Commercial Qt License Agreement"
+    author = "The Qt Company <https://www.qt.io/contact-us>"
+    url = "https://code.qt.io/cgit/qt/qtwebsockets.git"
+    description = (
+        "WebSocket is a web-based protocol designed to enable two-way communication "
+        "between a client application and a remote host. It enables the two entities "
+        "to send data back and forth if the initial handshake succeeds. WebSocket is "
+        "the solution for applications that struggle to get real-time data feeds with "
+        "less network latency and minimum data exchange."
+    )
+    topics = "qt", "qt6", "qtwebsockets", "WebSocket"
+    settings = "os", "compiler", "arch", "build_type"
+    # for referencing the version number and prerelease tag and dependencies info
+    exports = ".cmake.conf", "dependencies.yaml"
+    exports_sources = "*", "!conan*.*"
+    python_requires = f"qt-conan-common/{_get_qt_minor_version()}@qt/everywhere"
+    python_requires_extend = "qt-conan-common.QtLeafModule"
