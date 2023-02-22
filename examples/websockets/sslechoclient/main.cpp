5de3b2c69b6c3b51d48fef8f6306cbcea5faaeb2
Øystein Heskestad <oystein.heskestad@qt.io>
2023-02-22 18:05:41 +0100

    Make endpoint configurable for echoclient and sslechoclient examples
    Add handling of command line arguments for hostname and port.
    
    Task-number: QTBUG-110894
    Change-Id: I22d8f9e112cfb6c02b3e741c14720a0f28565984
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 99fdf11b808ab9b38239b04399a3fff21fa6ed35)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/websockets/sslechoclient/main.cpp
+++ examples/websockets/sslechoclient/main.cpp
@@ -1,13 +1,42 @@
 // Copyright (C) 2016 Kurt Pattyn <pattyn.kurt@gmail.com>.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 #include <QtCore/QCoreApplication>
+#include <QtCore/QCommandLineParser>
+#include <QtCore/QCommandLineOption>
 #include "sslechoclient.h"
 
 int main(int argc, char *argv[])
 {
+    using namespace Qt::Literals::StringLiterals;
     QCoreApplication a(argc, argv);
-    SslEchoClient client(QUrl(QStringLiteral("wss://localhost:1234")));
 
+    QCommandLineParser parser;
+    parser.setApplicationDescription("QtWebSockets example: sslechoclient");
+    parser.addHelpOption();
+
+    QCommandLineOption hostnameOption(
+            QStringList{ u"n"_s, u"hostname"_s },
+            QCoreApplication::translate("main", "Hostname [default: localhost]."), "hostname",
+            "localhost");
+    parser.addOption(hostnameOption);
+    QCommandLineOption portOption(QStringList{ u"p"_s, u"port"_s },
+                                  QCoreApplication::translate("main", "Port [default: 1234]."),
+                                  "port", "1234");
+    parser.addOption(portOption);
+
+    parser.process(a);
+    bool ok = true;
+    int port = parser.value(portOption).toInt(&ok);
+    if (!ok || port < 1 || port > 65535) {
+        qWarning("Port invalid, must be a number between 1 and 65535\n%s",
+                 qPrintable(parser.helpText()));
+        return 1;
+    }
+    QUrl url;
+    url.setScheme(u"wss"_s);
+    url.setHost(parser.value(hostnameOption));
+    url.setPort(port);
+    SslEchoClient client(url);
     Q_UNUSED(client);
 
     return a.exec();
