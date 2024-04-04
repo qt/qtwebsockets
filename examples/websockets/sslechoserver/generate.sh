05c2968afb23fb585af062931843509832f8d51f
Lucie Gérard <lucie.gerard@qt.io>
2024-04-04 18:38:17 +0200

    Change license for snippets and examples files
    According to QUIP-18 [1], all examples and snippets files should be
    LicenseRef-Qt-Commercial OR BSD-3-Clause
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Pick-to: 6.7
    Task-number: QTBUG-121787
    Change-Id: I1218766366d536e7db9fed24f2481bab3bea4d7d
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>

--- examples/websockets/sslechoserver/generate.sh
+++ examples/websockets/sslechoserver/generate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 # Copyright (C) 2024 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 openssl req -x509 -nodes -newkey rsa:4096 -keyout localhost.key -out localhost.cert -sha512 -days 365 -subj "/C=NO/ST=Oslo/L=Oslo/O=The Qt Project/OU=WebSockets/CN=localhost"
