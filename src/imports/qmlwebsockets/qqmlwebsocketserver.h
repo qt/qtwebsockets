3629511ce953ec2fd53d1f08fbc1e992351888e6
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-09-10 20:25:43 +0300

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit f5da9ab6a6b7ad08f5f0f84076d2e1890a6fb6a0.
    
    The revert is needed for the Qt 6.8 LTS opensource releases.
    They cannot contain commercial SPDX license identifiers.
    
    Task-number: QTLTSOSS-264
    Change-Id: I3a46419018620ece391e196088839f7b01d62908
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/imports/qmlwebsockets/qqmlwebsocketserver.h
+++ src/imports/qmlwebsockets/qqmlwebsocketserver.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Milian Wolff <milian.wolff@kdab.com>
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #ifndef QQMLWEBSOCKETSERVER_H
 #define QQMLWEBSOCKETSERVER_H
